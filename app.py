8c69fdc3a43998113ae4951a1a08eb476de5fe7a
Rajdeep Borgohain <150957746+rbgo404@users.noreply.github.com>
2024-03-25 12:07:31 +0530

    Update app.py

--- app.py
+++ app.py
@@ -14,7 +14,7 @@ class InferlessPythonModel:
         )
 
         # Define sampling parameters for model generation
-        self.sampling_params = SamplingParams(temperature=0.7, top_p=0.95, max_tokens=128)
+        self.sampling_params = SamplingParams(temperature=0.7, top_p=0.95, max_tokens=128,dtype="float16")
 
         # Initialize the LLM object
         self.llm = LLM(model=model_dir)
